09c658e834911e6e92511551dd12380d54cbffbd
Javier Martínez <jmartinez@develsystems.com>
2022-08-14 21:44:28 -0600

    Permissions array

--- web-app-multiservicio/src/app/modules/shared/pages/home/home.component.ts
+++ web-app-multiservicio/src/app/modules/shared/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { IPermissions } from '../Intefaces/IPermissions';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,9 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   rol: string | null = null;
+
+  public permissions!: IPermissions[];
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -19,6 +23,130 @@ export class HomeComponent implements OnInit {
     this.rol = localStorage.getItem('rol');
     if (!this.rol) {
       this.router.navigate(['/']);
+    }
+  }
+
+  getPermissions(rol: string): any {
+
+    switch(rol) {
+      case "Gerente General":
+        return this.permissions = [
+          {
+            permissionName: 'Home',
+            routerName: '/gerente-general/home'
+          },
+          {
+            permissionName: 'Auditoria Servicio',
+            routerName: 'auditoria-servicio'
+          },
+          {
+            permissionName: 'Analisis Repuesto',
+            routerName: 'analisis-repuesto'
+          },
+          {
+            permissionName: 'Empresa',
+            routerName: 'empresa'
+          },
+          {
+            permissionName: 'Dirección',
+            routerName: 'direccion'
+          },
+          {
+            permissionName: 'Municipio',
+            routerName: 'municipio'
+          },
+          {
+            permissionName: 'Departamento',
+            routerName: 'departamento'
+          },
+          {
+            permissionName: 'Tipo Telefono',
+            routerName: 'tipo-telefono'
+          },
+          {
+            permissionName: 'Telefono',
+            routerName: 'telefono'
+          },
+          {
+            permissionName: 'Persona',
+            routerName: 'persona'
+          },
+          {
+            permissionName: 'Tipo Persona',
+            routerName: 'tipo-persona'
+          },
+          {
+            permissionName: 'Tipo Repuesto',
+            routerName: 'tipo-repuesto'
+          },
+          {
+            permissionName: 'Repuesto',
+            routerName: 'repuesto'
+          },
+          {
+            permissionName: 'Equipo',
+            routerName: 'equipo'
+          },
+          {
+            permissionName: 'Servicio',
+            routerName: 'servicio'
+          },
+          {
+            permissionName: 'Perfil',
+            routerName: 'perfil'
+          }
+        ]
+        break;
+      
+      case "Trabajador Operacional":
+        return this.permissions = [
+          {
+            permissionName: 'Home',
+            routerName: 'home'
+          },
+          {
+            permissionName: 'Repuesto',
+            routerName: 'repuesto'
+          },
+          {
+            permissionName: 'Servicio',
+            routerName: 'servicio'
+          },
+          {
+            permissionName: 'Telefono',
+            routerName: 'telefono'
+          },
+          {
+            permissionName: 'Perfil',
+            routerName: 'perfil'
+          }
+        ]
+        break;
+
+      case "Cliente":
+        return this.permissions = [
+          {
+            permissionName: 'Home',
+            routerName: 'home'
+          },
+          {
+            permissionName: 'Equipo',
+            routerName: 'equipo'
+          },
+          {
+            permissionName: 'Servicio',
+            routerName: 'servicio'
+          },
+          {
+            permissionName: 'Telefono',
+            routerName: 'telefono'
+          },
+          {
+            permissionName: 'Perfil',
+            routerName: 'perfil'
+          }
+        ]
+        break;
     }
   }
 
