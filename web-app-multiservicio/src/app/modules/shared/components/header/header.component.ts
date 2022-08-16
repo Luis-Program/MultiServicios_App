ecf945f9d580737d5d557f0ac210a05076e94062
Javier Martínez <jmartinez@develsystems.com>
2022-08-15 21:21:38 -0600

    Notifications

--- web-app-multiservicio/src/app/modules/shared/components/header/header.component.ts
+++ web-app-multiservicio/src/app/modules/shared/components/header/header.component.ts
@@ -4,6 +4,7 @@ import { AuthB2cService } from 'src/app/services/auth-b2c.service';
 import { NotificacionService } from 'src/app/services/notificacion.service';
 import { PersonaRelaciones, UpdatePersonaDTO } from 'src/app/models/persona.model';
 import { PersonaService } from 'src/app/services/persona.service';
+import { Notificacion } from 'src/app/models/notificacion.model';
 
 @Component({
   selector: 'app-header',
@@ -12,6 +13,7 @@ import { PersonaService } from 'src/app/services/persona.service';
 })
 export class HeaderComponent implements OnInit {
 
+  protected notificaciones: Notificacion[] = [];
   protected persona: PersonaRelaciones | null = null;
   protected idPersona: string | null = null;
   protected rol: string | null = null;
@@ -26,6 +28,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit(): void {
     this.idPersona = localStorage.getItem('idPersona');
+    this.getNotificationsById(this.idPersona!);
     this.rol = localStorage.getItem('rol');
     if (this.idPersona && this.rol) {
       this.getOnePerson(this.idPersona);
@@ -65,5 +68,152 @@ export class HeaderComponent implements OnInit {
       case "Trabajador Operacional" : return this.router.navigate(['/trabajador/perfil']);
       case "Cliente"                : return this.router.navigate(['/cliente/perfil']);
     }
+  }
+
+  // NOTIFICATIONS
+
+  private getNotificationsById(idPersona: string) {
+    this.loading = true;
+    this.notificacionService.getAll(idPersona)
+      .subscribe(notifications => {
+        this.notificaciones = notifications;
+        console.log(notifications);
+        
+        this.loading = false;
+      });
+  }
+
+  protected goByNotification(notification: Notificacion) {
+    if (this.idPersona && this.rol) {
+      switch (this.rol) {
+        case 'Gerente General':
+          this.goByNotificationManager(notification);
+          break;
+          case 'Cliente':
+            this.goByNotificationClient(notification);
+            break;
+          case 'Trabajador Operacional':
+            this.goByNotificationWorker(notification);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  private goByNotificationManager(notification: Notificacion) {
+    if (notification.idTipoNotificacion) {
+      switch (notification.idTipoNotificacion) {
+        // Repuesto Escaso
+        case 1:
+          localStorage.setItem('idNoti', String(notification.notificacionId));
+          this.router.navigate(['gerente-general/repuesto']);
+          break;
+        // Nuevo Servicio Solicitado
+        case 3:
+          localStorage.setItem('idNoti', String(notification.notificacionId));
+          this.router.navigate(['gerente-general/servicio']);
+          break;
+        // Asignación de Trabajador
+        case 4:
+          localStorage.setItem('idNoti', String(notification.notificacionId));
+          this.router.navigate(['gerente-general/servicio']);
+          localStorage.setItem('servicio', 'update');
+          break;
+        // Activación de Equipo
+        case 6:
+          localStorage.setItem('idNoti', String(notification.notificacionId));
+          this.router.navigate(['gerente-general/equipo']);
+          break;
+        // Desactivación de Equipo
+        case 6:
+          localStorage.setItem('idNoti', String(notification.notificacionId));
+          this.router.navigate(['gerente-general/equipo']);
+          break;
+        // Servicio Finalizado
+        case 7:
+          localStorage.setItem('idNoti', String(notification.notificacionId));
+          this.router.navigate(['gerente-general/servicio']);
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  private goByNotificationClient(notification: Notificacion) {
+    if (notification.Tipo_Notificacion) {
+      if (notification.idTipoNotificacion) {
+        switch (notification.idTipoNotificacion) {
+          // Servicio Finalizado
+          case 7:
+            localStorage.setItem('idNoti', String(notification.notificacionId));
+            this.router.navigate(['cliente/servicio']);
+            break;
+          // Trabajador Asignado a su servicio
+          case 8:
+            localStorage.setItem('idNoti', String(notification.notificacionId));
+            this.router.navigate(['cliente/servicio']);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  private goByNotificationWorker(notification: Notificacion) {
+    if (notification.Tipo_Notificacion) {
+      if (notification.idTipoNotificacion === 2) {
+        localStorage.setItem('idNoti', String(notification.notificacionId));
+        this.router.navigate(['trabajador/servicio']);
+      }
+    }
+  }
+
+  protected setAllRead() {
+    if (this.idPersona) {
+      this.notificacionService.setReadNotifications(this.idPersona)
+        .subscribe(done => {
+          if (done) {
+            this.notificaciones = done;
+          }
+        });
+    }
+  }
+
+  protected setOneRead(idNotificacion: string | number) {
+    if (this.idPersona) {
+      this.notificacionService.update(idNotificacion, { visto: true })
+        .subscribe(notification => {
+          if (notification) {
+            const notificationIndex = this.notificaciones.findIndex(
+              (res) => res.idNotificacion === idNotificacion);
+            this.notificaciones[notificationIndex] = notification;
+          }
+        });
+    }
+  }
+
+  protected deleteAll() {
+    if (this.idPersona) {
+      this.notificacionService.deleteAll(this.idPersona)
+        .subscribe(done => {
+          if (done) {
+            this.notificaciones = [];
+          }
+        });
+    }
+  }
+
+  protected deleteOne(idNotificacion: string | number) {
+    this.notificacionService.delete(idNotificacion)
+      .subscribe(resp => {
+        if (resp) {
+          const notificationIndex = this.notificaciones.findIndex(
+            (res) => res.idNotificacion === idNotificacion);
+          this.notificaciones.splice(notificationIndex, 1);
+        }
+      });
   }
 }
