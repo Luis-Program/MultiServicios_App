fdb86289f964f9b32d17e35ad3f389c78b28b0bf
luiscaceres217@hotmail.es <Luis-Program>
2022-08-26 21:08:05 -0600

    Fixed schema <direccion> for create.

--- web-app-multiservicio/src/app/modules/manager/pages/direccion/direccion.component.ts
+++ web-app-multiservicio/src/app/modules/manager/pages/direccion/direccion.component.ts
@@ -40,8 +40,6 @@ export class DireccionComponent implements OnInit {
     this.direccionService.getAllWithRelations()
       .subscribe(addresses => {
         this.direcciones = addresses;
-        console.log(this.direcciones);
-        
         this.loading = false;
       });
   }
