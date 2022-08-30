d63629b1fda5019500ceadfaf02193231e1c73e0
Javier Martínez <jmartinez@develsystems.com>
2022-08-29 22:02:17 -0600

    Auditoria servicio finished

--- web-app-multiservicio/src/app/modules/manager/pages/auditoria-servicio/auditoria-servicio.component.ts
+++ web-app-multiservicio/src/app/modules/manager/pages/auditoria-servicio/auditoria-servicio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Auditoria_servicio } from 'src/app/models/auditoria_servicio.model';
 import { AuditoriaServicioService } from 'src/app/services/auditoria-servicio.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auditoria-servicio',
@@ -13,12 +14,16 @@ export class AuditoriaServicioComponent implements OnInit {
   protected loading = false;
   protected filter = "";
 
+  public Form!: FormGroup;
+
   constructor(
-    private auditoriaServicioService: AuditoriaServicioService
+    private auditoriaServicioService: AuditoriaServicioService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.getAllServiceAudit();
+    this.initForm();
   }
 
   private getAllServiceAudit(){
@@ -35,6 +40,51 @@ export class AuditoriaServicioComponent implements OnInit {
   }
 
   openModal(auditoria: Auditoria_servicio) {
-    console.log(auditoria);
+    this.initForm();
+    this.setForm(auditoria);
+  }
+
+  initForm() {
+    this.Form = this.fb.group({
+      dpiCliente                  : [''],
+      dpiTrabajador               : [''],
+      empresaCliente              : [''],
+      empresaClienteNit           : [''],
+      estado                      : [''],
+      fechaHora                   : [''],
+      fechaHoraAsignadoTrabajador : [''],
+      fechaHoraCreado             : [''],
+      fechaHoraFinalizado         : [''],
+      fechaHoraRealizar           : [''],
+      idAuditoriaServicio         : [''],
+      idServicio                  : [''],
+      nombreCliente               : [''],
+      nombreEquipo                : [''],
+      nombreTrabajador            : [''],
+      prioridad                   : [''],
+      tipoServicio                : [''],
+    })
+  }
+
+  setForm(auditoria: Auditoria_servicio) {
+    this.Form.setValue({
+      dpiCliente: auditoria.dpiCliente,
+      dpiTrabajador: auditoria.dpiTrabajador,
+      empresaCliente: auditoria.empresaCliente,
+      empresaClienteNit: auditoria.empresaClienteNit,
+      estado: auditoria.estado,
+      fechaHora: auditoria.fechaHora,
+      fechaHoraAsignadoTrabajador: auditoria.fechaHoraAsignadoTrabajador,
+      fechaHoraCreado: auditoria.fechaHoraCreado,
+      fechaHoraFinalizado: auditoria.fechaHoraFinalizado,
+      fechaHoraRealizar: auditoria.fechaHoraRealizar,
+      idAuditoriaServicio: auditoria.idAuditoriaServicio,
+      idServicio: auditoria.idServicio,
+      nombreCliente: auditoria.nombreCliente,
+      nombreEquipo: auditoria.nombreEquipo,
+      nombreTrabajador: auditoria.nombreTrabajador,
+      prioridad: auditoria.prioridad,
+      tipoServicio: auditoria.tipoServicio,
+    })
   }
 }
