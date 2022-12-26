fa49ba555fcd84665532aed22cd355c09fa7986e
luiscaceres217@hotmail.es <Luis-Program>
2022-12-25 18:06:26 -0600

    All

--- web-app-multiservicio/src/environments/environment.prod.ts
+++ web-app-multiservicio/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL_CLIENT: 'https://damp-spire-59848.herokuapp.com',
-  API_URL_EMPLOYEE: 'https://damp-spire-59848.herokuapp.com',
-  API_URL_MANAGER: 'https://damp-spire-59848.herokuapp.com',
+  API_URL_CLIENT: 'http://localhost:5003',
+  API_URL_EMPLOYEE: 'http://localhost:5002',
+  API_URL_MANAGER: 'http://localhost:5001',
 }
